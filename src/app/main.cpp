9a0aeb5f0b54b99c1c8b670a145824c559a629ea
pavelpa <pavelpa@888fa567-0be3-0310-be7d-ee6d82a910bf>
2010-01-13 08:40:31 +0000

    added application icon
    git-svn-id: http://svn.toastfreeware.priv.at/projects/confclerk/trunk@1004 888fa567-0be3-0310-be7d-ee6d82a910bf

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -4,9 +4,13 @@
 
 int main(int argc, char *argv[])
 {
+    Q_INIT_RESOURCE(icons);
+
     QApplication a(argc, argv);
+    QApplication::setWindowIcon(QIcon(":/icons/fosdem.png"));
 
     MainWindow w;
     w.show();
     return a.exec();
 }
+
