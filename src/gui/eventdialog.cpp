196452eafee269251bdc7d1dd5151eae0ca8d54a
pavelpa <pavelpa@888fa567-0be3-0310-be7d-ee6d82a910bf>
2010-01-20 19:49:28 +0000

    event dialog changes  - changed font/background colors  - title occupies more lines if it doesn't fit in one line
    git-svn-id: http://svn.toastfreeware.priv.at/projects/confclerk/trunk@1079 888fa567-0be3-0310-be7d-ee6d82a910bf

--- src/gui/eventdialog.cpp
+++ src/gui/eventdialog.cpp
@@ -11,24 +11,24 @@ EventDialog::EventDialog(const int &aEventId, QWidget *aParent)
     const int confId = 1;
     Event event = Event::getById(aEventId,confId);
 
-    abstract->setStyleSheet("background-color : transparent;");
-    description->setStyleSheet("background-color : transparent;");
+    //abstract->setStyleSheet("background-color : transparent;");
+    //description->setStyleSheet("background-color : transparent;");
 
     // use text color from 'title' QLabel
     QColor color = title->palette().color(QPalette::Active, QPalette::WindowText);
+    QColor bkgrColor = this->palette().color(QPalette::Active, QPalette::Background);
     QPalette p = abstract->palette();
-    //p.setColor(QPalette::Active, QPalette::Text, Qt::blue);
     p.setColor(QPalette::Active, QPalette::Text, color);
-    p.setColor(QPalette::Active, QPalette::WindowText, color);
+    p.setColor(QPalette::Active, QPalette::Base, bkgrColor);
     abstract->setPalette(p);
     description->setPalette(p);
 
     // set scrollbars color
-    QPalette p2 = description->verticalScrollBar()->palette();
-    p2.setColor(QPalette::Active, QPalette::Background, color);
-    //description->verticalScrollBar()->setStyleSheet("background-color : blue;");
-    abstract->verticalScrollBar()->setPalette(p2);
-    description->verticalScrollBar()->setPalette(p2);
+    //QPalette p2 = description->verticalScrollBar()->palette();
+    //p2.setColor(QPalette::Active, QPalette::Background, color);
+    ////description->verticalScrollBar()->setStyleSheet("background-color : blue;");
+    //abstract->verticalScrollBar()->setPalette(p2);
+    //description->verticalScrollBar()->setPalette(p2);
 
     title->setText(event.title());
     persons->setText(event.persons().join(" and "));
