f715db882322004cc4342fb577331b97f2037912
korrco <korrco@888fa567-0be3-0310-be7d-ee6d82a910bf>
2010-01-21 13:03:32 +0000

    activities tab implemented
    git-svn-id: http://svn.toastfreeware.priv.at/projects/confclerk/trunk@1089 888fa567-0be3-0310-be7d-ee6d82a910bf

--- src/mvc/eventmodel.cpp
+++ src/mvc/eventmodel.cpp
@@ -167,7 +167,7 @@ void EventModel::loadEvents(const QDate &aDate, int aConferenceId)
     if(Conference::getAll().count())
     {
         qDebug() << "Loading Conference Data: [" << Conference::getById(aConferenceId).title() << "] " << aDate;
-        mEvents = Event::getByDate(QDate(aDate.year(), aDate.month(), aDate.day()), aConferenceId, Event::START);
+        mEvents = Event::getByDate(QDate(aDate.year(), aDate.month(), aDate.day()), aConferenceId, "start");
     }
     createTimeGroups();
 }
