eea319b2b31872dce41076cb0ddc347a7e16c7af
pavelpa <pavelpa@888fa567-0be3-0310-be7d-ee6d82a910bf>
2010-01-27 12:06:32 +0000

    refactored Event 'details' dialog  - TODO: implement 'links' method(s) in Event and use it in the dialog
    git-svn-id: http://svn.toastfreeware.priv.at/projects/confclerk/trunk@1147 888fa567-0be3-0310-be7d-ee6d82a910bf

--- src/gui/eventdialog.cpp
+++ src/gui/eventdialog.cpp
@@ -7,48 +7,6 @@
 #include <alarm.h>
 #endif
 
-DetailsContainer::DetailsContainer(QWidget *aParent)
-    : QWidget(aParent)
-{
-    mAbstract.setWordWrap(true);
-    mDescription.setWordWrap(true);
-
-    QFont f = QLabel().font();
-    f.setBold(true);
-    f.setItalic(true);
-    mMainLayout = new QVBoxLayout(this);
-    QLabel *persons = new QLabel("Persons:");
-    persons->setFont(f);
-    mMainLayout->addWidget(persons);
-    mMainLayout->addWidget(&mPersons);
-    mMainLayout->addWidget(new QLabel("")); // spacer
-    QLabel *abstract = new QLabel("Abstract:");
-    abstract->setFont(f);
-    mMainLayout->addWidget(abstract);
-    mMainLayout->addWidget(&mAbstract);
-    mMainLayout->addWidget(new QLabel("")); // spacer
-    QLabel *description = new QLabel("Description:");
-    description->setFont(f);
-    mMainLayout->addWidget(description);
-    mMainLayout->addWidget(&mDescription);
-    setLayout(mMainLayout);
-}
-
-void DetailsContainer::setPersons(const QStringList &aPersons)
-{
-    mPersons.setText(aPersons.join(" and "));
-}
-
-void DetailsContainer::setAbstract(const QString &aAbstract)
-{
-    mAbstract.setText(aAbstract);
-}
-
-void DetailsContainer::setDescription(const QString &aDescription)
-{
-    mDescription.setText(aDescription);
-}
-
 EventDialog::EventDialog(const int &aEventId, QWidget *aParent)
     : QDialog(aParent)
     , mEventId(aEventId)
@@ -62,10 +20,9 @@ EventDialog::EventDialog(const int &aEventId, QWidget *aParent)
     Event event = Event::getById(mEventId,AppSettings::confId());
 
     title->setText(event.title());
-    mDetails.setPersons(event.persons());
-    mDetails.setAbstract(event.abstract());
-    mDetails.setDescription(event.description());
-    scrollArea->setWidget(&mDetails);
+    persons->setText(event.persons().join(" and "));
+    abstract->setText(event.abstract());
+    description->setText(event.description());
 
     connect(favouriteButton, SIGNAL(clicked()), SLOT(favouriteClicked()));
     connect(alarmButton, SIGNAL(clicked()), SLOT(alarmClicked()));
--- src/gui/eventdialog.h
+++ src/gui/eventdialog.h
@@ -5,20 +5,6 @@
 #include "ui_eventdialog.h"
 #include <event.h>
 
-class DetailsContainer : public QWidget
-{
-public:
-    DetailsContainer(QWidget *aParent = NULL);
-    void setPersons(const QStringList &aPersons);
-    void setAbstract(const QString &aAbstract);
-    void setDescription(const QString &aDescription);
-private:
-    QBoxLayout *mMainLayout;
-    QLabel mPersons;
-    QLabel mAbstract;
-    QLabel mDescription;
-};
-
 class EventDialog : public QDialog, Ui::EventDialog
 { 
     Q_OBJECT
@@ -32,7 +18,6 @@ signals:
     void eventHasChanged(int aEventId); // emited when user changes some event details, eg. sets it Favourite
 private:
     int mEventId;
-    DetailsContainer mDetails;
 };
 
 #endif /* EVENTDIALOG_H */
