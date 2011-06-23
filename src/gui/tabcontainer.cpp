9c753798c7b1063e079c2ceea549c5a7236e157a
gregoa <gregoa@888fa567-0be3-0310-be7d-ee6d82a910bf>
2011-06-23 15:36:05 +0000

    Update GPL blurb in source files.
    git-svn-id: http://svn.toastfreeware.priv.at/projects/confclerk/trunk@1275 888fa567-0be3-0310-be7d-ee6d82a910bf

--- src/gui/tabcontainer.cpp
+++ src/gui/tabcontainer.cpp
@@ -1,20 +1,20 @@
 /*
  * Copyright (C) 2010 Ixonos Plc.
  *
- * This file is part of fosdem-schedule.
+ * This file is part of ConfClerk.
  *
- * fosdem-schedule is free software: you can redistribute it and/or modify it
+ * ConfClerk is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation, either version 2 of the License, or (at your option)
  * any later version.
  *
- * fosdem-schedule is distributed in the hope that it will be useful, but
+ * ConfClerk is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  * more details.
  *
  * You should have received a copy of the GNU General Public License along with
- * fosdem-schedule.  If not, see <http://www.gnu.org/licenses/>.
+ * ConfClerk.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "tabcontainer.h"
 
