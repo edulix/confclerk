1dbcd20a52c4d6e8d4865d994b72e2adebf78e66
gregoa <gregoa@888fa567-0be3-0310-be7d-ee6d82a910bf>
2011-08-15 12:54:09 +0000

    .isEmpty() feels more Qtish then == ""
    git-svn-id: http://svn.toastfreeware.priv.at/projects/confclerk/trunk@1378 888fa567-0be3-0310-be7d-ee6d82a910bf

--- src/sql/sqlengine.cpp
+++ src/sql/sqlengine.cpp
@@ -112,7 +112,7 @@ void SqlEngine::addConferenceToDB(QHash<QString,QString> &aConference)
             // HACK
             // When city is empty, assign a dummy value. We probably want to find a way to change the database scheme ...
             // cf. #32
-            if (aConference["city"] == "") aConference["city"] = "n/a";
+            if (aConference["city"].isEmpty()) aConference["city"] = "n/a";
 
             QSqlQuery query(db);
             query.prepare("INSERT INTO CONFERENCE (title,url,subtitle,venue,city,start,end,days,"
